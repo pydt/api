3c53b53a2d6df97c06c8e2c64636c5b29b932171
Mike Rosack <mrosack@gmail.com>
2016-09-16 20:04:11 -0500

    game/turn schema

--- functions/user/games.js
+++ functions/user/games.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const User = require('../../lib/dynamoose/User.js');
+const Game = require('../../lib/dynamoose/Game.js');
+const _ = require('lodash');
+
 module.exports.handler = (event, context, cb) => {
-  cb(null, { message: 'TODO: output games for user...', userID: event.principalId });
+  console.log(event.principalId);
+
+  User.get(event.principalId).then(user => {
+    console.log(user);
+    let gameKeys = _.chain(user.games || [])
+      .filter(game => {
+        return !game.endDate;
+      })
+      .map(game => {
+        return { gameId: game.gameId }
+      })
+      .value();
+
+    if (gameKeys.length > 0) {
+      return Game.batchGet(gameKeys);
+    } else {
+      return [];
+    }
+  }).then(games => {
+    cb(null, games);
+  }).catch(err => {
+    cb(err);
+  });
 };
