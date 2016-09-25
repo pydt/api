491425f12a9510f9847d1f505919c9537db17451
Mike Rosack <mrosack@gmail.com>
2016-09-24 21:37:16 -0500

    added function to return active user steam profile

---
+++ functions/user/profile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const User = require('../../lib/dynamoose/User.js');
+const steam = require('../../lib/steam.js');
+const _ = require('lodash');
+
+module.exports.handler = (event, context, cb) => {
+  steam.getPlayerSummaries(event.principalId).then(response => {
+    let players = response.response.players;
+
+    if (players.length !== 1) {
+      throw new Error('Couldn\'t get user profile');
+    }
+
+    cb(null, players[0]);
+  })
+  .catch(err => {
+    cb(err);
+  });
+};
