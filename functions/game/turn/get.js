828df366ba8d6d0a598dd548750768d6f2fa5ee1
Mike Rosack <mrosack@gmail.com>
2016-09-25 10:45:24 -0500

    support for downloading turn

---
+++ functions/game/turn/get.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const config = require('../../../lib/config.js');
+const Game = require('../../../lib/dynamoose/Game.js');
+const GameTurn = require('../../../lib/dynamoose/GameTurn.js');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+module.exports.handler = (event, context, cb) => {
+  const gameId = event.path.gameId;
+
+  Game.get(gameId).then(game => {
+    if (game.currentPlayerSteamId !== event.principalId) {
+      throw new Error('It\'s not your turn!');
+    }
+
+  	cb(null, {
+      downloadUrl: s3.getSignedUrl('getObject', {
+        Bucket: config.RESOURCE_PREFIX + 'saves',
+        Key: GameTurn.createS3SaveKey(gameId, game.gameTurnRangeKey),
+        Expires: 60
+      })
+    });
+  })
+  .catch(err => {
+    cb(err);
+  });
+};
