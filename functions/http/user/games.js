5cfaf9c865d05b5851623603e4222e2d05f7bd72
Mike Rosack <mrosack@gmail.com>
2016-11-03 22:15:44 -0500

    optionally include s3 polling url until clients are updated

--- functions/http/user/games.js
+++ functions/http/user/games.js
@@ -19,6 +19,13 @@ module.exports.handler = (event, context, cb) => {
       return [];
     }
   }).then(games => {
+    if (event.headers.includepollurl) {
+      games = {
+        data: games,
+        pollUrl: 'https://' + common.config.RESOURCE_PREFIX + 'saves.s3.amazonaws.com/' + User.createS3GameCacheKey(event.principalId)
+      };
+    }
+
     cb(null, games);
   }).catch(err => {
     common.generalError(cb, err);
