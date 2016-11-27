4dc4339bc8f68386a360143945231ccb242e9632
Mike Rosack <mrosack@gmail.com>
2016-11-27 09:25:24 -0600

    new create game logic

--- functions/http/game/create.js
+++ functions/http/game/create.js
@@ -5,34 +5,25 @@ const discourse = require('../../../lib/discourse.js');
 const Game = require('../../../lib/dynamoose/Game.js');
 const User = require('../../../lib/dynamoose/User.js');
 const uuid = require('node-uuid');
-
+const _ = require('lodash');
 
 module.exports.handler = (event, context, cb) => {
+  let user;
+
   try {
-    const newGame = new Game({
-      gameId: uuid.v4(),
-      createdBySteamId: event.principalId,
-      currentPlayerSteamId: event.principalId,
-      players: [{
-        steamId: event.principalId
-      }],
-      displayName: event.body.displayName
-    });
+    User.get(event.principalId).then(_user => {
+      user = _user;
+      return Game.getGamesForUser(user);
+    }).then(games => {
+      let hasCreatedGame = _.some(games, game => {
+        return game.createdBySteamId === event.principalId;
+      });
 
-    discourse.addGameTopic(newGame).then(topic => {
-      if (topic) {
-        newGame.discourseTopicId = topic.topic_id;
+      if (hasCreatedGame) {
+        cb(new Error('[500] User has already created a game.'));
+      } else {
+        return createTheGame(user, event.body, cb);
       }
-
-      return Game.saveVersioned(newGame);
-    }).then(() => {
-      return User.get(event.principalId);
-    }).then(user => {
-      user.activeGameIds = user.activeGameIds || [];
-      user.activeGameIds.push(newGame.gameId);
-      return User.saveVersioned(user);
-    }).then(() => {
-      cb(null, newGame);
     }).catch(err => {
       common.generalError(cb, err);
     });
@@ -40,3 +31,35 @@ module.exports.handler = (event, context, cb) => {
     common.generalError(cb, err);
   }
 };
+
+//////
+
+function createTheGame(user, eventBody, cb) {
+  const newGame = new Game({
+    gameId: uuid.v4(),
+    createdBySteamId: user.steamId,
+    currentPlayerSteamId: user.steamId,
+    players: [{
+      steamId: user.steamId,
+      civType: eventBody.player1Civ
+    }],
+    displayName: eventBody.displayName,
+    description: eventBody.description,
+    slots: eventBody.slots,
+    humans: eventBody.humans
+  });
+
+  return discourse.addGameTopic(newGame).then(topic => {
+    if (topic) {
+      newGame.discourseTopicId = topic.topic_id;
+    }
+
+    return Game.saveVersioned(newGame);
+  }).then(() => {
+    user.activeGameIds = user.activeGameIds || [];
+    user.activeGameIds.push(newGame.gameId);
+    return User.saveVersioned(user);
+  }).then(() => {
+    cb(null, newGame);
+  });
+}
--- functions/http/user/games.js
+++ functions/http/user/games.js
@@ -3,21 +3,10 @@
 const common = require('../../../lib/common.js');
 const User = require('../../../lib/dynamoose/User.js');
 const Game = require('../../../lib/dynamoose/Game.js');
-const _ = require('lodash');
 
 module.exports.handler = (event, context, cb) => {
   User.get(event.principalId).then(user => {
-    let gameKeys = _.chain(user.activeGameIds || [])
-      .map(gameId => {
-        return { gameId: gameId }
-      })
-      .value();
-
-    if (gameKeys.length > 0) {
-      return Game.batchGet(gameKeys);
-    } else {
-      return [];
-    }
+    return Game.getGamesForUser(user);
   }).then(games => {
     cb(null, {
       data: games,
