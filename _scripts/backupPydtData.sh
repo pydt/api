ff96cf11b188d993bcd5cc84ad80c44e4055ae3c
Mike Rosack <mrosack@gmail.com>
2018-09-13 17:00:18 -0500

    add aws cli dynamo backup

--- _scripts/backupPydtData.sh
+++ _scripts/backupPydtData.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 DATE=`date +%Y%m%d%H%M`
-#DYNAMODB_ARCHIVE=dynamodb_${DATE}.tar.gz
-#WORKING_DIR=${HOME}/.pydtBackupTemp
-#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#rm -rf ${WORKING_DIR}
-
-#mkdir -p ${WORKING_DIR}/${DATE}
-
-#python ${SCRIPT_DIR}/dynamodump/dynamodump.py -m backup -s prod* -r us-east-1 --dumpPath ${WORKING_DIR}/${DATE}
-
-#tar -zcf ${WORKING_DIR}/${DYNAMODB_ARCHIVE} ${WORKING_DIR}/${DATE}
-
-#/usr/local/bin/aws s3 cp ${WORKING_DIR}/${DYNAMODB_ARCHIVE} s3://pydt-backup/${DATE}/${DYNAMODB_ARCHIVE} --storage-class STANDARD_IA --quiet
+/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-game --backup-name prod-civx-game-$DATE > /dev/null
+/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-game-turn --backup-name prod-civx-game-turn-$DATE > /dev/null
+/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-scheduled-job --backup-name prod-civx-scheduled-job-$DATE > /dev/null
+/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-user --backup-name prod-civx-user-$DATE > /dev/null
 
 /usr/local/bin/aws s3 ls s3://prod-civx-saves --recursive | while read -r line;
   do
@@ -29,5 +21,3 @@ DATE=`date +%Y%m%d%H%M`
         fi
     fi
   done;
-
-#rm -rf ${WORKING_DIR}
