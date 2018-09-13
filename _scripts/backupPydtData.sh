6244419970becf8bf79b230bfc8bf37398ca13e6
Mike Rosack <mrosack@gmail.com>
2018-09-13 16:45:40 -0500

    disable manual dynamo backup since dynamo has point in time restore now!

--- _scripts/backupPydtData.sh
+++ _scripts/backupPydtData.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 DATE=`date +%Y%m%d%H%M`
-DYNAMODB_ARCHIVE=dynamodb_${DATE}.tar.gz
-WORKING_DIR=${HOME}/.pydtBackupTemp
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#DYNAMODB_ARCHIVE=dynamodb_${DATE}.tar.gz
+#WORKING_DIR=${HOME}/.pydtBackupTemp
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm -rf ${WORKING_DIR}
+#rm -rf ${WORKING_DIR}
 
-mkdir -p ${WORKING_DIR}/${DATE}
+#mkdir -p ${WORKING_DIR}/${DATE}
 
-python ${SCRIPT_DIR}/dynamodump/dynamodump.py -m backup -s prod* -r us-east-1 --dumpPath ${WORKING_DIR}/${DATE}
+#python ${SCRIPT_DIR}/dynamodump/dynamodump.py -m backup -s prod* -r us-east-1 --dumpPath ${WORKING_DIR}/${DATE}
 
-tar -zcf ${WORKING_DIR}/${DYNAMODB_ARCHIVE} ${WORKING_DIR}/${DATE}
+#tar -zcf ${WORKING_DIR}/${DYNAMODB_ARCHIVE} ${WORKING_DIR}/${DATE}
 
-/usr/local/bin/aws s3 cp ${WORKING_DIR}/${DYNAMODB_ARCHIVE} s3://pydt-backup/${DATE}/${DYNAMODB_ARCHIVE} --storage-class STANDARD_IA --quiet
+#/usr/local/bin/aws s3 cp ${WORKING_DIR}/${DYNAMODB_ARCHIVE} s3://pydt-backup/${DATE}/${DYNAMODB_ARCHIVE} --storage-class STANDARD_IA --quiet
 
 /usr/local/bin/aws s3 ls s3://prod-civx-saves --recursive | while read -r line;
   do
