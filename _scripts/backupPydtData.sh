e1996e0b5b4a9973d89522ff2089d57b772eeb86
crichton <crichton@crichton.sacknet>
2020-07-05 12:56:40 +0000

    Update backup script

--- _scripts/backupPydtData.sh
+++ _scripts/backupPydtData.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 DATE=`date +%Y%m%d%H%M`
+TRUNCATE_DATE=`date -d "6 months ago" +%s`
 
-/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-game --backup-name prod-civx-game-$DATE > /dev/null
-/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-game-turn --backup-name prod-civx-game-turn-$DATE > /dev/null
-/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-scheduled-job --backup-name prod-civx-scheduled-job-$DATE > /dev/null
-/usr/local/bin/aws dynamodb create-backup --table-name prod-civx-user --backup-name prod-civx-user-$DATE > /dev/null
+aws dynamodb create-backup --table-name prod-civx-game --backup-name prod-civx-game-$DATE > /dev/null
+aws dynamodb create-backup --table-name prod-civx-game-turn --backup-name prod-civx-game-turn-$DATE > /dev/null
+aws dynamodb create-backup --table-name prod-civx-scheduled-job --backup-name prod-civx-scheduled-job-$DATE > /dev/null
+aws dynamodb create-backup --table-name prod-civx-user --backup-name prod-civx-user-$DATE > /dev/null
+aws dynamodb create-backup --table-name prod-civx-private-user-data --backup-name prod-civx-private-user-data-$DATE > /dev/null
+aws dynamodb create-backup --table-name prod-civx-websocket-connection --backup-name prod-civx-websocket-connection-$DATE > /dev/null
 
-/usr/local/bin/aws s3 ls s3://prod-civx-saves --recursive | while read -r line;
+aws dynamodb list-backups --table-name prod-civx-game | jq '.BackupSummaries[] | select(.BackupCreationDateTime < '$TRUNCATE_DATE').BackupArn' | xargs -L1 -I'{}' aws dynamodb delete-backup --backup-arn '{}' > /dev/null
+aws dynamodb list-backups --table-name prod-civx-game-turn | jq '.BackupSummaries[] | select(.BackupCreationDateTime < '$TRUNCATE_DATE').BackupArn' | xargs -L1 -I'{}' aws dynamodb delete-backup --backup-arn '{}' > /dev/null
+aws dynamodb list-backups --table-name prod-civx-scheduled-job | jq '.BackupSummaries[] | select(.BackupCreationDateTime < '$TRUNCATE_DATE').BackupArn' | xargs -L1 -I'{}' aws dynamodb delete-backup --backup-arn '{}' > /dev/null
+aws dynamodb list-backups --table-name prod-civx-user | jq '.BackupSummaries[] | select(.BackupCreationDateTime < '$TRUNCATE_DATE').BackupArn' | xargs -L1 -I'{}' aws dynamodb delete-backup --backup-arn '{}' > /dev/null
+aws dynamodb list-backups --table-name prod-civx-private-user-data | jq '.BackupSummaries[] | select(.BackupCreationDateTime < '$TRUNCATE_DATE').BackupArn' | xargs -L1 -I'{}' aws dynamodb delete-backup --backup-arn '{}' > /dev/null
+aws dynamodb list-backups --table-name prod-civx-websocket-connection | jq '.BackupSummaries[] | select(.BackupCreationDateTime < '$TRUNCATE_DATE').BackupArn' | xargs -L1 -I'{}' aws dynamodb delete-backup --backup-arn '{}' > /dev/null
+
+aws s3 ls s3://prod-civx-saves --recursive | while read -r line;
   do
     createDate=`echo $line|awk {'print $1" "$2'}`
     createDate=`date -d"$createDate" +%s`
@@ -17,7 +27,7 @@ DATE=`date +%Y%m%d%H%M`
         filename=`echo $line|awk {'print $4'}`
         if [[ $filename != "" ]]
           then
-            /usr/local/bin/aws s3 cp s3://prod-civx-saves/$filename s3://pydt-backup/$DATE/$filename --storage-class STANDARD_IA --quiet
+            aws s3 cp s3://prod-civx-saves/$filename s3://pydt-backup/$DATE/$filename --storage-class STANDARD_IA --only-show-errors
         fi
     fi
   done;
