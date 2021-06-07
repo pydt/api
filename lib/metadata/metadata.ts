e210ce8465c8ec698adf768fdb0cb87065836dd8
Mike Rosack <mrosack@gmail.com>
2021-06-06 20:25:15 -0500

    forgot to add old world to master game list

--- lib/metadata/metadata.ts
+++ lib/metadata/metadata.ts
@@ -3,9 +3,10 @@ import { CIV6_GAME } from './civGames/civ6';
 import { CIV5_GAME } from './civGames/civ5';
 import { BEYOND_EARTH_GAME } from './civGames/beyondEarth';
 import { CivGame, CivDef, RANDOM_CIV } from './civGame';
+import { OLD_WORLD_GAME } from './civGames/oldWorld';
 
 export const PYDT_METADATA: PydtMetadata = {
-  civGames: [CIV6_GAME, CIV5_GAME, BEYOND_EARTH_GAME],
+  civGames: [CIV6_GAME, CIV5_GAME, BEYOND_EARTH_GAME, OLD_WORLD_GAME],
   randomCiv: RANDOM_CIV
 };
 
