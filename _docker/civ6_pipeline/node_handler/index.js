b5caa330916f9f822322cd043566a3cc257eedd8
Mike Rosack <mrosack@gmail.com>
2023-04-25 19:19:39 +0000

    node function to run pipeline

--- _docker/civ6_pipeline/node_handler/index.js
+++ _docker/civ6_pipeline/node_handler/index.js
@@ -1,4 +1,50 @@
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand
+} = require('@aws-sdk/client-s3');
+const { execSync } = require('child_process');
+const fs = require('fs/promises');
+const path = require('path');
+
+const s3 = new S3Client({
+  region: 'us-east-1',
+});
+
+let callNum = 0;
+
 exports.handler = async function (event, context) {
-  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
-  return context.logStreamName;
+  const baseDir = `/tmp/${callNum++}`;
+
+  const { inputParams, outputParams } = JSON.parse(event.Records[0].Sns.Message);
+
+  const input = await s3.send(new GetObjectCommand(inputParams));
+  const inputFile = `${baseDir}/${inputParams.Key}`;
+  const outputDir = `${baseDir}/output`;
+
+  await fs.mkdir(path.dirname(inputFile), { recursive: true });
+  await fs.mkdir(outputDir, { recursive: true });
+
+  await fs.writeFile(inputFile, Buffer.from(await input.Body.transformToByteArray()));
+
+  execSync(`export NXF_VER=22.04.5
+
+  bin/nextflow \
+    run . \
+    -main-script workflows/civ6_pipeline/main_images-only.nf \
+    --input ${inputFile} \
+    --publishDir ${outputDir}`, {
+      cwd: '/civ6_pipeline'
+    });
+
+  const outputFiles = await fs.readdir(outputDir);
+
+  await s3.send(
+    new PutObjectCommand({
+      ...outputParams,
+      Body: await fs.readFile(`${outputDir}/${outputFiles[0]}`)
+    })
+  );
+
+  await fs.rmdir(baseDir, { recursive: true, force: true });
 };
