8d428a5f40ec64c18b38021cf6247ce62fae1582
Mike Rosack <mrosack@gmail.com>
2023-05-10 20:23:17 +0000

    switch to lambda node base image

--- _docker/civ6_pipeline/node_handler/entry_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-  exec /usr/local/bin/aws-lambda-rie /usr/bin/npx aws-lambda-ric $@
-else
-  exec /usr/bin/npx aws-lambda-ric $@
-fi  
--- _docker/civ6_pipeline/node_handler/index.js
+++ _docker/civ6_pipeline/node_handler/index.js
@@ -1,7 +1,18 @@
 const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { execSync } = require('child_process');
-const fs = require('fs-extra');
+const fs = require('fs/promises');
 const path = require('path');
+const Rollbar = require('rollbar');
+
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_API_KEY,
+  autoInstrument: false,
+  reportLevel: 'warning',
+  itemsPerMinute: 5,
+  payload: {
+    environment: process.env.SERVERLESS_STAGE,
+  },
+});
 
 const s3 = new S3Client({
   region: 'us-east-1'
@@ -10,38 +21,46 @@ const s3 = new S3Client({
 let callNum = 0;
 
 exports.handler = async function (event) {
-  const baseDir = `/tmp/${callNum++}`;
-
-  const { inputParams, outputParams } = JSON.parse(event.Records[0].Sns.Message);
-
-  const input = await s3.send(new GetObjectCommand(inputParams));
-  const inputFile = `${baseDir}/${inputParams.Key}`;
-  const outputDir = `${baseDir}/output`;
-
-  await fs.mkdir(path.dirname(inputFile), { recursive: true });
-  await fs.mkdir(outputDir, { recursive: true });
-
   try {
-    await fs.writeFile(inputFile, Buffer.from(await input.Body.transformToByteArray()));
+    const baseDir = `/tmp/${callNum++}`;
+
+    const { inputParams, outputParams } = JSON.parse(event.Records[0].Sns.Message);
+
+    const input = await s3.send(new GetObjectCommand(inputParams));
+    const inputFile = `${baseDir}/${inputParams.Key}`;
+    const outputDir = `${baseDir}/output`;
+
+    await fs.mkdir(path.dirname(inputFile), { recursive: true });
+    await fs.mkdir(outputDir, { recursive: true });
 
     try {
-      execSync(`/civ6_pipeline/target/native/civ6_save_renderer/parse_header/parse_header -i ${inputFile} -o ${outputDir}/header.yaml`);
-      execSync(`/civ6_pipeline/target/native/civ6_save_renderer/parse_map/parse_map -i ${inputFile} -o ${outputDir}/map.tsv`);
-      execSync(`/civ6_pipeline/target/native/civ6_save_renderer/plot_map/plot_map -y ${outputDir}/header.yaml -t ${outputDir}/map.tsv -o ${outputDir}/map.pdf`);
-      execSync(`/civ6_pipeline/target/native/civ6_save_renderer/convert_plot/convert_plot -i ${outputDir}/map.pdf -o ${outputDir}/map.png`);
-    } catch (err) {
-      throw new Error(`Processing failed!
-stdout: ${err.stdout.toString()}
-stderr: ${err.stderr.toString()}`);
-    }
+      await fs.writeFile(inputFile, Buffer.from(await input.Body.transformToByteArray()));
 
-    await s3.send(
-      new PutObjectCommand({
-        ...outputParams,
-        Body: await fs.readFile(`${outputDir}/map.png`)
-      })
-    );
-  } finally {
-    await fs.rmdir(baseDir, { recursive: true, force: true });
+      try {
+        execSync(`/civ6_pipeline/target/native/civ6_save_renderer/parse_header/parse_header -i ${inputFile} -o ${outputDir}/header.yaml`);
+        execSync(`/civ6_pipeline/target/native/civ6_save_renderer/parse_map/parse_map -i ${inputFile} -o ${outputDir}/map.tsv`);
+        execSync(`/civ6_pipeline/target/native/civ6_save_renderer/plot_map/plot_map -y ${outputDir}/header.yaml -t ${outputDir}/map.tsv -o ${outputDir}/map.pdf`);
+        execSync(`/civ6_pipeline/target/native/civ6_save_renderer/convert_plot/convert_plot -i ${outputDir}/map.pdf -o ${outputDir}/map.png`);
+      } catch (err) {
+        throw new Error(`Processing failed!
+  stdout: ${err.stdout.toString()}
+  stderr: ${err.stderr.toString()}`);
+      }
+
+      await s3.send(
+        new PutObjectCommand({
+          ...outputParams,
+          Body: await fs.readFile(`${outputDir}/map.png`)
+        })
+      );
+    } finally {
+      await fs.rm(baseDir, { recursive: true, force: true });
+    }
+  } catch (err) {
+    rollbar.error(err);
+    await new Promise(resolve => {
+      rollbar.wait(resolve);
+    });
+    throw err;
   }
 };
