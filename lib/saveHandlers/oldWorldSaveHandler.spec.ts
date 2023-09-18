6a06672c53e0a1f281e66da420d2ecd78d63c235
photogineer <photogineer@users.noreply.github.com>
2023-09-18 16:41:36 -0400

    Bugfix: Handle newer version format (#56)

--- lib/saveHandlers/oldWorldSaveHandler.spec.ts
+++ lib/saveHandlers/oldWorldSaveHandler.spec.ts
@@ -26,6 +26,26 @@ describe('OldWorldSaveHandler', () => {
     expect(handler.civData[1].isCurrentTurn).to.eq(true);
   });
 
+  it('should parse correctly (newer format)', () => {
+    const buffer = fs.readFileSync('testdata/saves/oldWorld/Turn1B.zip');
+    const handler = new OldWorldSaveHandler(buffer);
+
+    expect(handler.gameSpeed).to.eq('');
+    expect(handler.gameTurn).to.eq(1);
+    expect(handler.mapFile).to.eq('MAPCLASS_CoastalRainBasin');
+    expect(handler.mapSize).to.eq('MAPSIZE_MEDIUM');
+
+    expect(handler.parsedDlcs.length).to.eq(0);
+
+    expect(handler.civData.length).to.eq(5);
+    expect(handler.civData[0].isCurrentTurn).to.eq(false);
+    expect(handler.civData[0].leaderName).to.eq('NATION_ASSYRIA');
+    expect(handler.civData[0].password).to.eq('');
+    expect(handler.civData[0].playerName).to.eq('matt');
+    expect(handler.civData[0].type).to.eq(ActorType.HUMAN);
+    expect(handler.civData[1].isCurrentTurn).to.eq(true);
+  });
+
   it('should update civs correctly', () => {
     const buffer = fs.readFileSync('testdata/saves/oldWorld/Turn1.zip');
     let handler = new OldWorldSaveHandler(buffer);
@@ -47,6 +67,33 @@ describe('OldWorldSaveHandler', () => {
     expect(handler.civData.length).to.eq(2);
     expect(handler.civData[0].isCurrentTurn).to.eq(false);
     expect(handler.civData[0].leaderName).to.eq('NATION_ROME');
+    expect(handler.civData[0].password).to.eq('');
+    expect(handler.civData[0].playerName).to.eq('playerName');
+    expect(handler.civData[0].type).to.eq(ActorType.HUMAN);
+    expect(handler.civData[1].isCurrentTurn).to.eq(true);
+  });
+
+  it('should update civs correctly (newer format)', () => {
+    const buffer = fs.readFileSync('testdata/saves/oldWorld/Turn1B.zip');
+    let handler = new OldWorldSaveHandler(buffer);
+
+    const civ = handler.civData[0];
+    //civ.password = 'password';
+    civ.playerName = 'playerName';
+    //civ.type = ActorType.AI;
+
+    handler = new OldWorldSaveHandler(handler.getData());
+
+    expect(handler.gameSpeed).to.eq('');
+    expect(handler.gameTurn).to.eq(1);
+    expect(handler.mapFile).to.eq('MAPCLASS_CoastalRainBasin');
+    expect(handler.mapSize).to.eq('MAPSIZE_MEDIUM');
+
+    expect(handler.parsedDlcs.length).to.eq(0);
+
+    expect(handler.civData.length).to.eq(5);
+    expect(handler.civData[0].isCurrentTurn).to.eq(false);
+    expect(handler.civData[0].leaderName).to.eq('NATION_ASSYRIA');
     expect(handler.civData[0].password).to.eq('');
     expect(handler.civData[0].playerName).to.eq('playerName');
     expect(handler.civData[0].type).to.eq(ActorType.HUMAN);
