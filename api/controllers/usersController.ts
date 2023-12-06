9dc0af0b38e33f576c432a5c55cff09b445f1b6f
Mike Rosack <mrosack@gmail.com>
2023-12-06 04:45:36 -0600

    add batch user endpoint

--- api/controllers/usersController.ts
+++ api/controllers/usersController.ts
@@ -1,32 +1,27 @@
-import { Get, Response, Route, Tags } from 'tsoa';
+import { Get, Request, Route, Tags } from 'tsoa';
 import { IUserRepository, USER_REPOSITORY_SYMBOL } from '../../lib/dynamoose/userRepository';
 import { inject, provideSingleton } from '../../lib/ioc';
 import { User } from '../../lib/models';
-import { ErrorResponse } from '../framework';
-import * as moment from 'moment';
+import { HttpRequest, HttpResponseError } from '../framework';
 
 @Route('users')
 @Tags('user')
 @provideSingleton(UsersController)
 export class UsersController {
-  private cachedUsers?: User[];
-
-  private userCacheDate?: Date;
-
   constructor(@inject(USER_REPOSITORY_SYMBOL) private userRepository: IUserRepository) {}
 
-  @Response<ErrorResponse>(401, 'Unauthorized')
-  @Get('')
-  public async all(): Promise<User[]> {
-    if (
-      !this.cachedUsers ||
-      !this.userCacheDate ||
-      moment(this.userCacheDate).isBefore(moment().subtract(5, 'minutes'))
-    ) {
-      this.cachedUsers = await this.userRepository.usersWithTurnsPlayed();
-      this.userCacheDate = new Date();
+  @Get('{steamIds}')
+  public async byIds(@Request() request: HttpRequest, steamIds: string): Promise<User[]> {
+    const splitIds = steamIds.split(',');
+
+    if (!steamIds || !splitIds.length) {
+      throw new HttpResponseError(400, `steamIds must be provided`);
     }
 
-    return this.cachedUsers;
+    if (splitIds.length > 20) {
+      throw new HttpResponseError(400, `too many!`);
+    }
+
+    return await this.userRepository.batchGet(splitIds);
   }
 }
